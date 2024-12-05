23c513320f8039bec199fe1073e5e357f7320997
iarivera <iariveradev@outlook.com>
2024-12-05 14:17:14 -0500

    scatter plot displays

--- src/Graph3.js
+++ src/Graph3.js
@@ -12,29 +12,55 @@ function Graph3({ data }) {
     const svg = d3.select(svgRef.current);
     svg.selectAll('*').remove();
 
-    const margin = { top: 20, right: 30, bottom: 70, left: 70 };
+    const margin = { top: 20, right: 100, bottom: 70, left: 70 };
     const genderGroups = ['Male', 'Female', 'Other'];
 
-    const preparedData = data;
-
+    const parsedData = data.map(d => ({
+      Study_Hours: parseFloat(d.Study_Hours),
+      Sleep_Quality: parseFloat(d.Sleep_Quality),
+    }))
     
     console.log(d => d.Sleep_Quality)
+    const color = d3.scaleOrdinal()
+      .domain(genderGroups)
+      .range(['#1f77b4', '#ff7f0e', '#2ca02c']);
 
     const x = d3
       .scaleLinear()
-      .domain([0, d3.max(preparedData, d => d.Study_Hours)])
+      .domain([0, d3.max(parsedData, d => d.Study_Hours)])
       .range([margin.left, width - margin.right])
     
     const y = d3
       .scaleLinear()
-      .domain([0, d3.max(preparedData, d => d.Sleep_Quality)])
+      .domain([0, d3.max(parsedData, d => d.Sleep_Quality)])
       .nice()
       .range([height - margin.bottom, margin.top]);
 
+    svg 
+      .append('g')
+      .selectAll("dot")
+      .data(data)
+      .enter()
+      .append("circle")
+        .attr("cx", function (d) { return x(d.Study_Hours) })
+        .attr("cy", function (d) { return y(d.Sleep_Quality) })
+        .attr("r", 2)
+        .style("fill", d => color(d.Gender));
+
+    svg
+      .append('g')
+      .attr('transform', `translate(0, ${height - margin.bottom})`)
+      .call(d3.axisBottom(x));
+
+    svg
+      .append('g')
+      .attr('transform', `translate(${margin.left}, 0)`)
+      .call(d3.axisLeft(y));
+
     svg
       .append('text')
-      .attr('transform', `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom(x))
+      .attr('text-anchor', 'middle')
+      .attr('transform', `translate(${width/2},${height - margin.bottom + 35})`)
       .text("Study Hours")
 
     svg
@@ -43,11 +69,7 @@ function Graph3({ data }) {
       .attr('transform', `translate(${margin.left / 2},${height / 2})rotate(-90)`)
       .text("Sleep Quality")
 
-    const color = d3.scaleOrdinal()
-      .domain(genderGroups)
-      .range(['#1f77b4', '#ff7f0e', '#2ca02c']);
-
-    svg
+    /*svg
       .append('g')
       .selectAll('rect')
       .data(genderGroups)
@@ -59,16 +81,17 @@ function Graph3({ data }) {
       .attr('height', 10)
       .attr('fill', d => color(d));
 
-    svg 
+    svg
       .append('g')
-      .selectAll("dot")
-      .data(preparedData)
+      .selectAll('text')
+      .data(genderGroups)
       .enter()
-      .append("circle")
-        .attr("cx", function (d) { return x(d.Study_Hours) })
-        .attr("cy", function (d) { return y(d.Sleep_Quality) })
-        .attr("r", 5)
-        .style("fill", d => color(d));
+      .append('text')
+      .attr('x', (d, i) => width - margin.right - 185 + i * 70)
+      .attr('y', 20)
+      .text(d => d)
+      .style('font-size', '12px')
+      .attr('alignment-baseline', 'middle');*/
   
   }, [data]);
 
